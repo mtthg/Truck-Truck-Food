2ca17cb5e2ed9e2e1d10401ff8de600706464f47
mtthg <verseau858@gmail.com>
2011-02-13 16:24:38 -0500

    first commit

---
+++ src/com/heroku/bft/Homescreen.java
@@ -0,0 +1,13 @@
+package com.heroku.bft;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homescreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
