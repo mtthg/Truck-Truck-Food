31fcdd5ab3de590e2549902b49126e8351479fef
mtthg <verseau858@gmail.com>
2011-02-13 17:09:34 -0500

    added notworking location code

--- src/com/heroku/bft/Homescreen.java
+++ src/com/heroku/bft/Homescreen.java
@@ -1,13 +1,44 @@
 package com.heroku.bft;
 
 import android.app.Activity;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class Homescreen extends Activity {
+	public LocationManager locMgr = null;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    
+        locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
+        locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+        		10000, 10000.0f,
+        		onLocationChange);
+        
     }
-}
+    
+    LocationListener onLocationChange=new LocationListener() {
+    	public void onLocationChanged(Location location) {
+    	if (state.weather!=null) {
+    	new FetchForecastTask(state).execute(location);
+    	}
+    	else {
+    		Log.w(getClass().getName(), "Unable to fetch forecast – no WeatherBinder");
+    				}
+    				}
+    				public void onProviderDisabled(String provider) {
+    				// required for interface, not used
+    				}
+    				public void onProviderEnabled(String provider) {
+    				// required for interface, not used
+    				}
+    				public void onStatusChanged(String provider, int status,
+    				Bundle extras) {
+    				// required for interface, not used
+    				}
+    				};
+    	}
