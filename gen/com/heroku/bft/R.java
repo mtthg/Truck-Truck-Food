77f95b55df0f11123790f485c6de7a3973f5d927
jli <crastalli@gmail.com>
2011-02-17 03:40:18 -0500

    first page added

--- gen/com/heroku/bft/R.java
+++ gen/com/heroku/bft/R.java
@@ -11,10 +11,18 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int bg_vertical=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int logo_new=0x7f020003;
+    }
+    public static final class id {
+        public static final int adventure=0x7f050001;
+        public static final int find_truck=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int frontscreen=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
