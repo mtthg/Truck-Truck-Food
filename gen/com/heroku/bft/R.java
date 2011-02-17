f56c1f523cf5562ffa18604acd5308e021c451c0
mtthg <verseau858@gmail.com>
2011-02-17 03:10:17 -0500

    directions file added

--- gen/com/heroku/bft/R.java
+++ gen/com/heroku/bft/R.java
@@ -13,8 +13,24 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int Driving=0x7f050003;
+        public static final int Walking=0x7f050002;
+        public static final int estimatedtime=0x7f050001;
+        public static final int getdirections=0x7f05000a;
+        public static final int hours=0x7f050009;
+        public static final int mapview1=0x7f05000b;
+        public static final int step1=0x7f050004;
+        public static final int step2=0x7f050005;
+        public static final int step3=0x7f050006;
+        public static final int step4=0x7f050007;
+        public static final int step5=0x7f050008;
+        public static final int truckname=0x7f050000;
+        public static final int viewmenu=0x7f05000c;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int directions=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
---
+++ src/com/heroku/bft/Directions.java
@@ -0,0 +1,19 @@
+package com.heroku.bft;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.content.Intent;
+
+public class Directions extends Activity {
+    	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.directions);
+
+
+    }   
+}
