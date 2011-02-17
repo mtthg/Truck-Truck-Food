4da772ee894b7bcbc90ff0bd93dce638fd985cef
mtthg <verseau858@gmail.com>
2011-02-17 04:42:57 -0500

    lots of stuff

--- gen/com/heroku/bft/R.java
+++ gen/com/heroku/bft/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int babyfoodtruck=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int Driving=0x7f050003;
--- src/com/heroku/bft/GoogleMaps.java
+++ src/com/heroku/bft/GoogleMaps.java
@@ -1,25 +1,32 @@
 package com.heroku.bft;
 
+import java.util.List;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class GoogleMaps extends MapActivity  {
     
-	private MapView mapView;
+	MapView mapView;
     private MapController mc;
-
+    List<Overlay> mapOverlays;
+    Drawable drawable;
+    ItemizedOverlay itemizedOverlay;
 	
     /** Called when the activity is first created. */
     @Override
@@ -28,26 +35,32 @@ public class GoogleMaps extends MapActivity  {
         setContentView(R.layout.main);
         
         mapView = (MapView) findViewById(R.id.mapview1);
+        mapView.setBuiltInZoomControls(true);
         mc = mapView.getController();
+        
         Button getDirections = (Button) findViewById(R.id.getdirections); 
         getDirections.setOnClickListener(directionsScreen);
         
-    	
-
-        String coordinates[] = {"40.747778", "-73.985556"};
+        String coordinates[] = {"42.359844", "-71.059050"};
         double lat = Double.parseDouble(coordinates[0]);
         double lng = Double.parseDouble(coordinates[1]);
         
-        //use loc.getLongitude() and loc.getLatitude() in place of lat and lng
-     
         GeoPoint p = new GeoPoint( //represents a geographical location
             (int) (lat * 1E6), 
             (int) (lng * 1E6));
-
-        mc.animateTo(p); //move map to specific location
-        mc.setZoom(17); 
+        
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.babyfoodtruck);
+        itemizedOverlay = new ItemizedOverlay(drawable);
+        
+        OverlayItem overlayitem = new OverlayItem(p, "", "");
+        
+        mc.animateTo(p);
+        mc.setZoom(19); 
         mapView.setSatellite(true);
-        mapView.invalidate();        //what does invalidate do?     
+        itemizedOverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedOverlay);
+        mapView.invalidate();  
     }
 
 	public View.OnClickListener directionsScreen=new View.OnClickListener() {
